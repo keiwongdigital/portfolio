fe49db6282cd3578076483be41a5de199bbc4321
keiwongdigital <84902589+keiwongdigital@users.noreply.github.com>
2021-12-07 01:14:06 +0800

    0612_09

---
+++ interview.js
@@ -0,0 +1,46 @@
+ref: 
+https://www.cakeresume.com/resources/how-to-prepare-for-a-junior-ux-designer-job
+https://www.cakeresume.com/resources/how-to-prepare-a-job-interview
+
+
+就是好好研究職缺的 Job Description，並把關鍵字填進你的 Short Bio 中
+
+
+
+自我介紹:
+
+你好....我叫黄紹基, 英文單字阿K, 
+
+
+離職reason？
+
+
+面試問題:
+為什麼你這樣設計？
+你的優缺點是什麼？
+請用幾句話介紹一下我們的產品。
+
+
+面試時作品集介紹:
+1.互動過程
+
+展現 UX 設計思維的
+
+
+
+
+Point:
+UX 設計師則重「解決問題」的能力, 使用者「用起來的感覺」
+UI 「看起來的感覺」，後面經過UX分析同研究, 最後用UI呈現结果outcome
+
+
+公司、組織帶來什麼樣的價值
+
+
+
+最後提問:
+1.「如果最後沒能進入貴公司，最大的因素是？您認為我哪方面需要再加強？」
+2.「請問這個職缺是怎麼產生的？」
+
+UX 在每間企業的工作內容都會有所不同，可能會注重研究，也可能會比較注重解決問題
+你地公司...
