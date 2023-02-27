f0a4b93800deeffee20d8c8a3d2156df65b3ad8e
keiwongdigital <84902589+keiwongdigital@users.noreply.github.com>
2023-02-27 16:30:47 +0800

    270223_01

--- javascript/landing.js
+++ javascript/landing.js
@@ -112,8 +112,8 @@ class TextScramble {
     for (let i = 0; i < length; i++) {
       const from = oldText[i] || ""
       const to = newText[i] || ""
-      const start = Math.floor(Math.random() * 260)
-      const end = start + Math.floor(Math.random() * 140)
+      const start = Math.floor(Math.random() * 220)
+      const end = start + Math.floor(Math.random() * 120)
       this.queue.push({ from, to, start, end })
     }
     cancelAnimationFrame(this.frameRequest)
